4d8df01736eb8b1407a7b7ee9688886c5abb81e4
rustyff12 <russellfrankpitt@googlemail.com>
2023-12-21 22:16:03 +0100

    Added vigenere

--- cipher.py
+++ cipher.py
@@ -1,16 +1,23 @@
 # Caesar cipher
 text = "Hello World"
-
 shift = 3
-alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-encrypted_text = ""
 
-for char in text.lower():
-    if char == " ":
-        encrypted_text += char
-    else:
-        index = alphabet.find(char)
-        new_index = (index + shift) % 26
-        encrypted_text += alphabet[new_index]
-        print("char:", char, "encrypted text:", encrypted_text)
+def caesar(message, offset):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    encrypted_text = ""
+
+    for char in message.lower():
+        if char == " ":
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print("plain text:", message)
+    print("encrypted text:", encrypted_text)
+
+
+caesar(text, shift)
+caesar(text, 13)
