2a0aaeb1103451012db027283da2d53dd28c587c
rustyff12 <russellfrankpitt@googlemail.com>
2023-12-21 19:02:52 +0100

    Initial commit

---
+++ cipher.py
@@ -0,0 +1,16 @@
+# Caesar cipher
+text = "Hello World"
+
+shift = 3
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+encrypted_text = ""
+
+for char in text.lower():
+    if char == " ":
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % 26
+        encrypted_text += alphabet[new_index]
+        print("char:", char, "encrypted text:", encrypted_text)
